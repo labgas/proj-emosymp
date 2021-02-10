a302cc323795403abb7a45363dcef58b45785844
lukasvo76 <60989355+lukasvo76@users.noreply.github.com>
2021-02-09 22:25:35 -0500

    improvements and study-specific adaptations to LaBGAS_create_first_level_CANlab_tools_folder_structure.m

--- LaBGAS_create_first_level_CANlab_tools_folder_structure.m
+++ LaBGAS_create_first_level_CANlab_tools_folder_structure.m
@@ -15,67 +15,79 @@
 % @(#)% LaBGAS_create_first_level_CANlab_tools_folder_structure.m     v1.        
 % last modified: 2021/02/09
 
-%% define directories and make new first level directory
-rootdir='C:\Users\lukas\Dropbox (Personal)\proj-SERT-fMRI';
-cd(rootdir);
-mkdir('firstlevel_CANlab_tools');
+%% define directories (absolute path) and make new first level directory with model subdirectory
+% comment out mkdir lines if you already manually created these dirs
+rootdir='C:\Users\lukas\Dropbox (Dartmouth College)\fMRI_emotion_Giao\BIDS';
+% cd(rootdir);
+% mkdir('firstlevel');
 firstleveldir=fullfile(rootdir,'firstlevel');
-firstlevelcanlabdir=fullfile(rootdir,'firstlevel_CANlab_tools');
-fmriprepdir=fullfile(rootdir,'derivatives\fmriprep');
+% cd(firstleveldir);
+% mkdir('model_1_CANlab_classic_GLM');
+modeldir=fullfile(firstleveldir,'model_1_CANlab_classic_GLM');
+derivdir=fullfile(rootdir,'derivatives\fmriprep');
+runnames={'run-1';'run-2';'run-3';'run-4';'run-5';'run-6'}; % define names of run directories to be written in derivdir
 
-%% get subject folder names from derivatives\fmriprep
-cd(fmriprepdir);
+%% get subject directory names from derivdir
+cd(derivdir);
 subjs=dir('sub-*');
 idx=[subjs.isdir]';
 subjs={subjs(idx).name}';
 
-%% define anonymous function sm to write folder structure with cell array of folder names as input
+%% define anonymous function sm to write folder structure with cell array of directory names as input
 sm=@(x)spm_mkdir(x); % defines spm_mkdir as an anonymous function sm
 
-%% write folder structure in new first level directory
-cd(firstlevelcanlabdir);
+%% write folder structure in new model directory
+cd(modeldir);
 cellfun(sm,subjs); % applies function sm to all cells of subjs
 
-%% loop over subjects to copy and move the onsets, noise_regs, functional scans, and fmriprep confound files to subdirectories per run (which equals condition in our case)
-for i=1:length(subjs)
-    subjfirstleveldir=fullfile(firstleveldir,subjs{i});
-    subjfmriprepdir=fullfile(fmriprepdir,subjs{i},'func');
-    cd(subjfirstleveldir);
+%% loop over subjects to copy and move the onsets, noise_regs, functional scans, and fmriprep confound files to subdirectories per run
+for i=13:size(subjs,1)
+    subjmodeldir=fullfile(modeldir,subjs{i});
+    subjderivdir=fullfile(derivdir,subjs{i},'func');
+    cd(subjderivdir);
     onset_files=ls('onset*.mat');
     noise_files=ls('noise*.txt');
-    [nr_noise_files,~] = size(noise_files);
-    [nr_onset_files,~] = size(onset_files);
-%     for j=1:nr_noise_files
-%         copyfile(fullfile(subjfirstleveldir,noise_files(j,:)),fullfile(subjfmriprepdir,noise_files(j,:)));
-%         copyfile(fullfile(subjfirstleveldir,onset_files(j,:)),fullfile(subjfmriprepdir,onset_files(j,:)));
-%     end
-    cd(subjfmriprepdir);
-%     noise_mat_files=ls('noise*.mat');
-%     [nr_noise_mat_files,~] = size(noise_mat_files);
-%     for k=1:nr_noise_mat_files
-%         delete(noise_mat_files(k,:));
-%     end
-    conds={'neg';'neu';'pos'};
-    cellfun(sm,conds);
-    niigz_files=ls('s6*.nii.gz');
-    niigz_files=cellstr(niigz_files);
-    idx=~contains(niigz_files,"rest");
-    niigz_files=niigz_files(idx);
-    fmriprepconf_files=ls('sub*.tsv');
-    fmriprepconf_files=cellstr(fmriprepconf_files);
-    idx=~contains(fmriprepconf_files,"rest");
-    fmriprepconf_files=fmriprepconf_files(idx);
-    if ~isequal(nr_noise_files,nr_onset_files,length(niigz_files),length(fmriprepconf_files))
+    nr_noise_files = size(noise_files,1);
+    nr_onset_files = size(onset_files,1);
+    cellfun(sm,runnames);
+    niigz_files=cellstr(ls('s6*.nii.gz'));
+    fmriprepconf_files=cellstr(ls('sub*.tsv'));
+    if ~isequal(nr_noise_files,nr_onset_files,size(niigz_files,1),size(fmriprepconf_files,1))
         error('number of onset, noise, functional, and fmriprep confound files does not match');
     else
-        for m=1:length(niigz_files)
-            gunzip(niigz_files{m});
+        for j=1:size(niigz_files,1)
+            gunzip(niigz_files{j});
             niifile=ls('s6*.nii');
-            rundir=fullfile(subjfmriprepdir,conds{m});
-            movefile(fullfile(subjfmriprepdir,niifile),fullfile(rundir,niifile));
-            movefile(fullfile(subjfmriprepdir,onset_files(m,:)),fullfile(rundir,onset_files(m,:)));
-            movefile(fullfile(subjfmriprepdir,noise_files(m,:)),fullfile(rundir,noise_files(m,:)));
-            copyfile(fullfile(subjfmriprepdir,fmriprepconf_files{m,:}),fullfile(rundir,fmriprepconf_files{m,:}));
+                if contains(niifile,runnames{j})
+                    rundir=fullfile(subjderivdir,runnames{j});
+                    movefile(fullfile(subjderivdir,niifile),fullfile(rundir,niifile));
+                    movefile(fullfile(subjderivdir,onset_files(j,:)),fullfile(rundir,onset_files(j,:)));
+                    movefile(fullfile(subjderivdir,noise_files(j,:)),fullfile(rundir,noise_files(j,:)));
+                    copyfile(fullfile(subjderivdir,fmriprepconf_files{j,:}),fullfile(rundir,fmriprepconf_files{j,:}));
+                    delete(niigz_files{j});
+                elseif contains(niifile,runnames{j+1})
+                    warning(strcat(runnames{j},'_missing_in_',subjs{i}));
+                    rundir=fullfile(subjderivdir,runnames{j+1});
+                    movefile(fullfile(subjderivdir,niifile),fullfile(rundir,niifile));
+                    movefile(fullfile(subjderivdir,onset_files(j,:)),fullfile(rundir,onset_files(j,:)));
+                    movefile(fullfile(subjderivdir,noise_files(j,:)),fullfile(rundir,noise_files(j,:)));
+                    copyfile(fullfile(subjderivdir,fmriprepconf_files{j,:}),fullfile(rundir,fmriprepconf_files{j,:}));
+                    delete(niigz_files{j});
+                elseif contains(niifile,runnames{j+2})
+                    rundir=fullfile(subjderivdir,runnames{j+2});
+                    movefile(fullfile(subjderivdir,niifile),fullfile(rundir,niifile));
+                    movefile(fullfile(subjderivdir,onset_files(j,:)),fullfile(rundir,onset_files(j,:)));
+                    movefile(fullfile(subjderivdir,noise_files(j,:)),fullfile(rundir,noise_files(j,:)));
+                    copyfile(fullfile(subjderivdir,fmriprepconf_files{j,:}),fullfile(rundir,fmriprepconf_files{j,:}));
+                    delete(niigz_files{j});
+                elseif contains(niifile,runnames{j+3})
+                    rundir=fullfile(subjderivdir,runnames{j+3});
+                    movefile(fullfile(subjderivdir,niifile),fullfile(rundir,niifile));
+                    movefile(fullfile(subjderivdir,onset_files(j,:)),fullfile(rundir,onset_files(j,:)));
+                    movefile(fullfile(subjderivdir,noise_files(j,:)),fullfile(rundir,noise_files(j,:)));
+                    copyfile(fullfile(subjderivdir,fmriprepconf_files{j,:}),fullfile(rundir,fmriprepconf_files{j,:}));
+                    delete(niigz_files{j});
+                end
         end
     end
 end
